959bb450977d90ba15b71431c05ecec6f29fc095
chenfh <425348908@qq.com>
2019-09-25 15:37:14 +0800

    eureka新增

---
+++ eureka/src/main/java/main/EurekaApplication.java
@@ -0,0 +1,17 @@
+package main;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/**
+ * @author chenfh
+ * @date 2019-09-25 15:31
+ */
+@SpringBootApplication
+@EnableEurekaServer
+public class EurekaApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaApplication.class, args);
+    }
+}
